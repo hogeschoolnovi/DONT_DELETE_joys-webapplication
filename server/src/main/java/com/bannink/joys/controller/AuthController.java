9519a8f51104fe5b53d956d279739ccaf33674cf
amy-bannink <amy.bannink@baroes.nl>
2020-10-29 15:16:51 +0100

    backend security basics

---
+++ server/src/main/java/com/bannink/joys/controller/AuthController.java
@@ -0,0 +1,43 @@
+package com.bannink.joys.controller;
+
+import com.bannink.joys.payload.request.*;
+import com.bannink.joys.payload.response.*;
+import com.bannink.joys.service.AuthorizationService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Uitleg over CrossOrigin en CORS:
+ * https://medium.com/@baphemot/understanding-cors-18ad6b478e2b
+ *
+ * Gebruik in Spring-boot (op controller en globally)
+ * https://www.tutorialspoint.com/spring_boot/spring_boot_cors_support.htm
+ *
+ * Zoals je hieronder ziet, kun je ook op klasse-niveau een adres configureren. Iaw alle methodes hieronder, hebben
+ * /api/auth voor de link staan.
+ */
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    AuthorizationService authorizationService;
+
+    @PostMapping("/signin")
+    public ResponseEntity<JwtResponse> authenticateUser(@RequestBody LoginRequest loginRequest) {
+        return authorizationService.authenticateUser(loginRequest);
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<MessageResponse> registerUser(@RequestBody SignupRequest signUpRequest) {
+        return authorizationService.registerUser(signUpRequest);
+    }
+
+}
---
+++ server/src/main/java/com/bannink/joys/domain/ERole.java
@@ -0,0 +1,7 @@
+package com.bannink.joys.domain;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
